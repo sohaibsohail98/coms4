137cfd696df01533da6ab9415fd9d59f5a1a90cd
S Sohail <ssohail@spartaglobal.com>
2020-07-01 14:41:27 +0100

    pythonupdate5

--- Python/Day2Python.py
+++ Python/Day2Python.py
@@ -116,22 +116,52 @@
 
 #Syntax of Tuple: we use () to create a Tuple
 
-user_details = ("name", "dob", "passport number")
-print(user_details)
-
-print(user_details[1])
+# user_details = ("name", "dob", "passport number")
+# print(user_details)
+#
+# print(user_details[1])
 
 #Convert the tuple into an list
 #add your name into the string at 0 index
 #display the list
 
-user_details_list =list(user_details) #Convert the tuple into an list
-print(user_details_list)
+# user_details_list =list(user_details) #Convert the tuple into an list
+# print(user_details_list)
+#
+# print(type(user_details_list))
+#
+# user_details_list.insert(0,"James")   #add your name into the string at 0 index
+# print(user_details_list) #display the list
 
-print(type(user_details_list))
+#Dictionaries:
+#This is more dynamic than tuples and lists, useful tool within coding
+#simple concept of key value pairs
 
-user_details_list.insert(0,"James")   #add your name into the string at 0 index
-print(user_details_list) #display the list
+# syntax: {} to create a dictionary
+                   #   key: value
+student_record = { "name": "sohaib",
+                   "stream": "DevOps",
+                   "completed_lessons": 5,
+                   "completed_lesson_names": ["strings", "tuples", "variables"]
+
+}
+
+# print(student_record)
+# print(type(student_record))
+# print(sorted(student_record)) #Returning the list in ascending order
+# print(student_record.values())
+
+# student_record["completed_lesson_names"] = 3
+#
+# new_list = student_record["completed_lesson_names"]
+# print(new_list)
+
+print(student_record.items())
+print(student_record["stream"]) #Fetch the value of stream
+
+print(student_record["completed_lesson_names"][2])
+
+
 
 
 
